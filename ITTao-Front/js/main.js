2487f9f8d0034bb230438441c3773dc5a1ce5c0a
lihuabest <lihuabest@gmail.com>
2014-05-09 18:28:36 +0800

    首页基础架构完成 header部分样式完成

--- ITTao-Front/js/main.js
+++ ITTao-Front/js/main.js
@@ -2,11 +2,38 @@ require.config({
     baseUrl: 'js',
     paths: {
         jquery: 'lib/jquery/jquery',
+        easing: 'lib/jquery/jquery.easing.1.3',
         livereload: 'livereload'
     }
 });
 
-require(['jquery'], function($) {
-    console.log('load jquery');
-//    livereload();
+require(['jquery', 'easing'], function(jQuery, sasing) {
+    var $ = jQuery;
+    $.easing.def = "easeOutBounce";
+
+    $(function() {
+        var search = $('.search');
+        search.children('input').on('focus', function() {
+            var that = $(this);
+            that.animate({
+                width: '150px'
+            }, 900);
+            that.siblings('i')
+                   .removeClass('glyphicon-search')
+                   .addClass('glyphicon-ok')
+                   .bind('click', function() {
+                     console.log(that.val());
+                });
+        });
+        search.on('focusout', function() {
+            console.log(111);
+            $(this).children('input')
+                .animate({
+                width: '40px'
+            }, 900)
+                .siblings('i')
+                .removeClass('glyphicon-ok')
+                .addClass('glyphicon-search');
+        });
+    });
 });
