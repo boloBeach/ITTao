d1b75225f0ad49aaf23371d25e4e2b5d6089f2f0
lihuabest <lihuabest@gmail.com>
2014-05-09 23:36:41 +0800

    '更新了slide幻灯效果'

--- ITTao-Front/js/main.js
+++ ITTao-Front/js/main.js
@@ -9,7 +9,7 @@ require.config({
 
 require(['jquery', 'easing'], function(jQuery, sasing) {
     var $ = jQuery;
-    $.easing.def = "easeOutBounce";
+    jQuery.easing.def = "easeOutBounce";
 
     $(function() {
         var search = $('.search');
@@ -35,5 +35,80 @@ require(['jquery', 'easing'], function(jQuery, sasing) {
                 .removeClass('glyphicon-ok')
                 .addClass('glyphicon-search');
         });
+
+        var slide = $('.slide'),
+            slide_li = slide.find('li'),
+            slide_index = 0,
+            slide_li_now,
+            slide_li_move;
+        slide.children('span').on('click', function() {
+//            jQuery.easing.def = "swing";
+            var that = $(this);
+            if(that.hasClass('left')) {
+                if(slide_index == slide_li.size() - 1) {
+                    slide_index = 0
+                } else {
+                    slide_index++;
+                }
+                slide_li_now = slide_li.filter('.show');
+                slide_li_move = slide_li.eq(slide_index);
+                slide_li_move.addClass('showNoneZ');
+                if(!slide_li_now.is(':animated')) {
+                    slide_li_now.animate({
+                        left: '-500px'
+                    }, 1000, 'swing', function() {
+                        slide_li_now.removeClass('show').css({
+                            left: 0
+                        });
+                        slide_li_move.addClass('show').removeClass('showNoneZ');
+                    });
+                }
+            } else {
+                if(slide_index == 0) {
+                    slide_index = slide_li.size() - 1;
+                } else {
+                    slide_index--;
+                }
+                slide_li_now = slide_li.filter('.show');
+                slide_li_move = slide_li.eq(slide_index);
+                slide_li_move.addClass('showNoneZ');
+                if(!slide_li_now.is(':animated')) {
+                    slide_li_now.animate({
+                        left: '500px'
+                    }, 1000, 'swing', function() {
+                        slide_li_now.removeClass('show').css({
+                            left: 0
+                        });
+                        slide_li_move.addClass('show').removeClass('showNoneZ');
+                    });
+                }
+            }
+        });
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
