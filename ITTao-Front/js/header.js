8cfdca8b60b70da87288b82eb52993f7473519a5
lihuabest <lihuabest@gmail.com>
2014-05-14 07:35:20 +0800

    更改js加载器为seajs

---
+++ ITTao-Front/js/header.js
@@ -0,0 +1,59 @@
+define(function(require, exports, module) {
+//    var $ = require('jquery');
+    var easing = require('easing');
+
+    jQuery.easing.def = "easeOutBounce";
+
+    $(function() {
+        var search = $('.search');
+        search.children('input').on('focus', function() {
+            var that = $(this);
+            that.animate({
+                width: '150px'
+            }, 900);
+            that.siblings('i')
+                .removeClass('glyphicon-search')
+                .addClass('glyphicon-ok')
+                .bind('click', function() {
+                    console.log(that.val());
+                });
+        });
+        search.on('focusout', function() {
+            console.log(111);
+            $(this).children('input')
+                .animate({
+                    width: '40px'
+                }, 900)
+                .siblings('i')
+                .removeClass('glyphicon-ok')
+                .addClass('glyphicon-search');
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ITTao-Front/js/main.2.js
@@ -0,0 +1,84 @@
+define(function(require, exports, module) {
+//    var $ = require('jquery');
+    var easing = require('easing');
+
+    jQuery.easing.def = "easeOutBounce";
+
+    $(function() {
+        var slide = $('.slide'),
+            slide_li = slide.find('li'),
+            slide_index = 0,
+            slide_li_now,
+            slide_li_move;
+        slide.children('span').on('click', function() {
+//            jQuery.easing.def = "swing";
+            var that = $(this);
+            if(that.hasClass('left')) {
+                if(slide_index == slide_li.size() - 1) {
+                    slide_index = 0
+                } else {
+                    slide_index++;
+                }
+                slide_li_now = slide_li.filter('.show');
+                slide_li_move = slide_li.eq(slide_index);
+                slide_li_move.addClass('showNoneZ');
+                if(!slide_li_now.is(':animated')) {
+                    slide_li_now.animate({
+                        left: '-500px'
+                    }, 1000, 'swing', function() {
+                        slide_li_now.removeClass('show').css({
+                            left: 0
+                        });
+                        slide_li_move.addClass('show').removeClass('showNoneZ');
+                    });
+                }
+            } else {
+                if(slide_index == 0) {
+                    slide_index = slide_li.size() - 1;
+                } else {
+                    slide_index--;
+                }
+                slide_li_now = slide_li.filter('.show');
+                slide_li_move = slide_li.eq(slide_index);
+                slide_li_move.addClass('showNoneZ');
+                if(!slide_li_now.is(':animated')) {
+                    slide_li_now.animate({
+                        left: '500px'
+                    }, 1000, 'swing', function() {
+                        slide_li_now.removeClass('show').css({
+                            left: 0
+                        });
+                        slide_li_move.addClass('show').removeClass('showNoneZ');
+                    });
+                }
+            }
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
